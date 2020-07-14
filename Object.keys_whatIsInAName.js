a73b8be3745354596cdff18f056c98121af95282
sunjus <sunju.shin1108@gmail.com>
2020-07-14 21:25:04 +0300

    Create Object.keys_whatIsInAName.js

---
+++ Object.keys_whatIsInAName.js
@@ -0,0 +1,29 @@
+function whatIsInAName(collection, source) {
+  var arr = [];
+  // Only change code below this line
+  let srcKeys = Object.keys(source);
+
+  return collection.filter(function (obj) {
+    for (let i = 0; i < srcKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(srcKeys[i]) ||
+        obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+  arr.push(collection.filter());
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" },
+  ],
+  { last: "Capulet" }
+);
