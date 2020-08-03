9d03d6558ff1b5e808ae1ad38d39fad1c5cbe4a5
sunjus <sunju.shin1108@gmail.com>
2020-08-03 17:39:13 +0300

    Create manageState.jsx

---
+++ manageState.jsx
@@ -0,0 +1,43 @@
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      messages: [],
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+      messages: this.state.messages,
+    });
+  }
+
+  submitMessage() {
+    this.setState({
+      input: "",
+      messages: [...this.state.messages, this.state.input],
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+
+        <input
+          onChange={this.handleChange.bind(this)}
+          value={this.state.input}
+        />
+        <button onClick={this.submitMessage.bind(this)}>Submit</button>
+
+        <ul>
+          {this.state.messages.map((text, index) => {
+            return <li key={index}>{text}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
